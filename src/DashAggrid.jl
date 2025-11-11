00b52186dd1942e479fcf161433d14de56a4e66d
skilgore <skilgore@landmarktx.com>
2025-11-10 21:18:34 -0600

    bump version to 0.2.3

--- src/DashAggrid.jl
+++ src/DashAggrid.jl
@@ -3,7 +3,7 @@ module DashAggrid
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.2.2"
+const version = "0.2.3"
 
 include("jl/agjs_agchartsjs.jl")
 include("jl/agjs_aggridjs.jl")
