79b2741c563e7e7a06c209ec25843acc675e94f7
Scott Kilgore <kilgore.scott@gmail.com>
2025-11-10 22:05:59 -0600

    Describe SSRM docs + build script fix

---
+++ src/DashAggridJs.jl
@@ -0,0 +1,37 @@
+
+module DashAggridJs
+using Dash
+
+const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
+const version = "0.2.3"
+
+include("jl/agjs_agchartsjs.jl")
+include("jl/agjs_aggridjs.jl")
+
+function __init__()
+    DashBase.register_package(
+        DashBase.ResourcePkg(
+            "dash_aggrid_js",
+            resources_path,
+            version = version,
+            [
+                DashBase.Resource(
+    relative_package_path = "dash_aggrid_js.min.js",
+    external_url = nothing,
+    dynamic = nothing,
+    async = nothing,
+    type = :js
+),
+DashBase.Resource(
+    relative_package_path = "dash_aggrid_js.min.js.map",
+    external_url = nothing,
+    dynamic = true,
+    async = nothing,
+    type = :js
+)
+            ]
+        )
+
+    )
+end
+end
